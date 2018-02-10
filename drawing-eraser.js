d78197cff0cc3a8e2e740f2ccbb6be5174f2b5b5
francoeduardo <35854414+francoeduardo@users.noreply.github.com>
2018-02-09 17:11:53 -0800

    Create drawing-eraser.js

---
+++ drawing-eraser.js
@@ -0,0 +1,31 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+var diameter = 10;
+
+function draw() { 
+  if (mouseIsPressed) {
+    ellipse(mouseX, mouseY, diameter);
+  }
+}
+
+function keyPressed() {
+  print(key);
+  if (key == 'R') {
+    fill(244,67,54);
+  } else if (key == 'G') {
+    fill(139, 195, 74);
+  } else if (key == 'B') {
+    fill(3, 169, 244); 
+  } else if (key == 'O') {
+    fill (255,152,0);
+	} else if (key == 'P') {
+    fill (233,30,99);
+  } else if (key == 'C') {
+    fill (0,188,212)
+	} else if (key == 'E') {
+    fill (255);
+    noStroke();
+  }
+}
