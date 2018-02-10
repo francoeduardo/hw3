716def0f904d4300f78a1aae5be174260e42411a
francoeduardo <35854414+francoeduardo@users.noreply.github.com>
2018-02-09 17:09:18 -0800

    Create drawing-with-diameter.js

---
+++ drawing-with-diameter.js
@@ -0,0 +1,38 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+//var diameter = 10;
+
+
+function draw() { 
+  if (mouseIsPressed) {
+    ellipse(mouseX, mouseY, diameter);
+  }
+}
+
+function keyPressed() {
+  print(key);
+  if (key == '1'){
+    var diameter = 10;
+  } else if (key == '2'){
+    var diameter = 20;
+  } else if (key == '3'){
+    var diameter = 30;
+  }
+}
+
+  if (key == 'R') {
+    fill(244,67,54);
+  } else if (key == 'G') {
+    fill(139, 195, 74);
+  } else if (key == 'B') {
+    fill(3, 169, 244); 
+  } else if (key == 'O') {
+    fill (255,152,0);
+	} else if (key == 'P') {
+    fill (233,30,99);
+  } else if (key == 'C') {
+    fill (0,188,212);
+  }
+}
